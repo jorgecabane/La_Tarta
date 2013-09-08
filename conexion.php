a385445b7d580e2cfadf210d5caad05956a74ed2
Cesar Gonzalez <cgonzb@gmail.com>
2013-09-08 17:23:55 -0300

    configuracion de conexion

--- conexion.php
+++ conexion.php
@@ -1,10 +1,16 @@
 <?php
  
 // datos para la coneccion a mysql
-define('DB_SERVER','localhost');
+//define('DB_SERVER','localhost');
+//define('DB_NAME','La_Tarta');
+//define('DB_USER','root');
+//define('DB_PASS','');
+
+// datos para la coneccion a mysql
+define('DB_SERVER','200.54.168.133');
 define('DB_NAME','La_Tarta');
 define('DB_USER','root');
-define('DB_PASS','');
+define('DB_PASS','pepito.P789');
  
     $con = mysql_connect(DB_SERVER,DB_USER,DB_PASS);
     mysql_select_db(DB_NAME,$con);

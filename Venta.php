87ab802d7615fc5960da5a0216350720ccf626c6
nicoride93 <nicoride93@gmail.com>
2013-09-08 17:23:18 -0400

    Update Venta.php

--- Venta.php
+++ Venta.php
@@ -1,22 +1,6 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-        <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
-        <!--<script src="http://code.jquery.com/jquery-1.9.1.js"></script>-->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script><!-- JQUERY-->
-        <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
-
-    </head>
-    <body style="background: rgb(30,87,153); /* Old browsers */
-          background: -moz-linear-gradient(-45deg,  rgba(30,87,153,1) 0%, rgba(41,137,216,1) 50%, rgba(32,124,202,1) 51%, rgba(125,185,232,1) 100%); /* FF3.6+ */
-          background: -webkit-gradient(linear, left top, right bottom, color-stop(0%,rgba(30,87,153,1)), color-stop(50%,rgba(41,137,216,1)), color-stop(51%,rgba(32,124,202,1)), color-stop(100%,rgba(125,185,232,1))); /* Chrome,Safari4+ */
-          background: -webkit-linear-gradient(-45deg,  rgba(30,87,153,1) 0%,rgba(41,137,216,1) 50%,rgba(32,124,202,1) 51%,rgba(125,185,232,1) 100%); /* Chrome10+,Safari5.1+ */
-          background: -o-linear-gradient(-45deg,  rgba(30,87,153,1) 0%,rgba(41,137,216,1) 50%,rgba(32,124,202,1) 51%,rgba(125,185,232,1) 100%); /* Opera 11.10+ */
-          background: -ms-linear-gradient(-45deg,  rgba(30,87,153,1) 0%,rgba(41,137,216,1) 50%,rgba(32,124,202,1) 51%,rgba(125,185,232,1) 100%); /* IE10+ */
-          background: linear-gradient(135deg,  rgba(30,87,153,1) 0%,rgba(41,137,216,1) 50%,rgba(32,124,202,1) 51%,rgba(125,185,232,1) 100%); /* W3C */
-          filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1e5799', endColorstr='#7db9e8',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-          ">
+<?php
+include "header.php"
+?>
         <div class="container-fluid">
             <div class="span12">
                 <div class="row-fluid">
@@ -26,8 +10,8 @@
                     </div>
                 </div>
                 <div id="codigo" class="row-fluid  well well-small">
-                    <?php //require()..?>
-                    <input type="text" id="barra" class="input-xlarge" placeholder="Ingrese nombre o utilice lector">
+                    <?php include "input.php"?>
+                    
 
                 </div>
 
@@ -37,16 +21,7 @@
                     </div>
                     <div class="row-fluid">
                         <div class="span9" id="carro">
-                            <div class="alert alert-info">
-
-                                <span class="label label-info"><strong>Coca-Cola Lata 350cc</strong></span>
-
-                                Precio <span class="label">500</span> <a class="close" data-dismiss="alert" href="#">&times;</a>
-
-                            </div>
-                            <div class="alert alert-info"><span class="label label-info"><strong>Papas fritas Lays 80gr</strong></span> Precio <span class="label">250</span> <a class="close" data-dismiss="alert" href="#">&times;</a></div>
-                            <div class="alert alert-info"><span class="label label-info"><strong>Chocolate Trencito 100gr</strong></span> Precio <span class="label">300</span> <a class="close" data-dismiss="alert" href="#">&times;</a></div>
-                            <div class="alert alert-info"><span class="label label-info"><strong>Agua Mineral Cachantún 500cc</strong></span> Precio <span class="label">400</span> <a class="close" data-dismiss="alert" href="#">&times;</a></div>
+                            
                         </div>
 
 
@@ -82,43 +57,6 @@
 
     </body>
 </html>
-<script>
-    var pepe = $('#barra');
-    $(document).ready(function() {
-        pepe.focus();
-    });
-    pepe.change(function() {
-        var id = $(this).val();
-        if ($.isNumeric(id)) {
-            $('#error').html('<center><img id="loading" src="gif.gif"><center>');
-            $.ajax({
-                "url": "Buscar_Insumo.php",
-                "data": {"id": id},
-                "type": "post",
-                "async": true,
-                "success": function(prod) {
-                    if (prod == 0) {
-                        $('#error').html('').html('<div class="alert alert-warning">Error!</div>');
-                        $('#barra').select();
-                    }//if
-                    else
-                    {
-                        alert(prod);
-                        //manejo del Jason!
-                        $('#error').html('');
-                        var pepe = $('#barra');
-                        $(document).ready(function() {
-                            pepe.focus();
-                        });
-                    }//else
-
-                }//success
-            });//ajax
-        }
-        //alert(id);
-    });
-</script>
-
 
 
 

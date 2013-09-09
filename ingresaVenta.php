b1627d135aa7ec612f7874324ead054ace6295ec
Cesar Gonzalez <cgonzb@gmail.com>
2013-09-09 00:02:55 -0300

    arreglin de ingreso de venta

--- ingresaVenta.php
+++ ingresaVenta.php
@@ -7,86 +7,77 @@ require_once 'conexion.php';
 // primero insertar un detalle
 //el id va a ser un array
 //
-$id2 = array(12345,123455);//$_POST['id'];
-$medioDePago = 'e';//$_POST['medioPago'];
-$valor = 5000;//$_POST['valor'];
-$sessionid= 1;//$_SESSION['id'];          // ingresar en alguna parte que se quede guardado el session id del usuario
+$id2 = array(12345, 123455); //$_POST['id'];
+$medioDePago = 'e'; //$_POST['medioPago'];
+$valor = 5000; //$_POST['valor'];
+$sessionid = 1; //$_SESSION['id'];          // ingresar en alguna parte que se quede guardado el session id del usuario
 $usuario = 'admin'; //$_SESSION["usuario"];
 //$_SESSION['usuario'] = 'admin';
 
 
-if (isset($_POST['profesor']) ) {
-  //  $profesor=$_POST['profesor'];
-    //insertar venta for each id                         luego detalle             luego rebajar el inventario for each id
-    
-
-    //    $query = "Insert into Ventas ('Valor','fecha','medioDePago','Usuarios_idUsuario','Profesores_idProfesores')
-	//	Values ($valor,NOW(),'$medioDePago',$sessionid,$profesor)";
-
-        //echo $query;
-     //   $resultado = mysql_query($query) or die(mysql_error());
-        
-	//	$query3 = "select MAX(idVentas) from Ventas";
-	//	$resultado3 = mysql_query($query3) or die(mysql_error());
-		
-	//	foreach ($id2 as $id) {
-		
-		// falta un for each para ingresar por cada id ingresado ---------------------------------------------------------------------------------------
-    //   $id = trimm($id);
-	//	$query2 = "Insert into detalle ('Insumos_idInsumos','Ventas_idVentas')
-      //  Values ($id,$resultado3)";
-	//	$resultado2 = mysql_query($query2) or die(mysql_error());
-  //  }//foreAach           
-}
-else 
-{
-$query = "Insert into Ventas 
+if (isset($_POST['profesor'])) {
+//    $profesor = $_POST['profesor'];
+//    //insertar venta for each id                         luego detalle             luego rebajar el inventario for each id
+//    $query = "Insert into Ventas ('Valor','fecha','medioDePago','Usuarios_idUsuario','Profesores_idProfesores')
+//	Values ($valor,NOW(),'$medioDePago',$sessionid,$profesor)";
+//    //echo $query;
+//    $resultado = mysql_query($query) or die(mysql_error());
+//    $query3 = "select MAX(idVentas) from Ventas";
+//    $resultado3 = mysql_query($query3) or die(mysql_error());
+//    foreach ($id2 as $id) {
+////falta un for each para ingresar por cada id ingresado ---------------------------------------------------------------------------------------
+//        $id = trimm($id);
+//        $query2 = "Insert into detalle ('Insumos_idInsumos','Ventas_idVentas')
+//  Values ($id,$resultado3)";
+//        $resultado2 = mysql_query($query2) or die(mysql_error());
+    // }//foreAach
+} //si hay profesor
+else {
+    $cantProductos = count($id2);
+    $query = "Insert into Ventas
 		Values (NULL,$valor,$sessionid,NULL,NOW(),'$medioDePago')";
-		
-		
+    //echo $query;
+    $resultado = mysql_query($query) or die(mysql_error());
 
-        //echo $query;
-        $resultado = mysql_query($query) or die(mysql_error());
-		if($resultado){
+    if ($resultado) {
         $query3 = "select MAX(idVentas) from Ventas";
+        $resultado3 = mysql_query($query3) or die(mysql_error());
 
-		$resultado3 = mysql_query($query3) or die(mysql_error());
-											if($resultado3)
-											{
-												$ultimoid= mysql_fetch_assoc($resultado3);
-										$ultimoid2= $ultimoid['MAX(idVentas)'];
-										print_r ($ultimoid2);
-										
-										foreach ($id2 as $id) {
-										
-										
-										// falta un for each para ingresar por cada id ingresado ---------------------------------------------------------------------------------------
-										$query10="select idInsumos from Insumos where Codigo=$id";
-										echo $query10;
-										
-										$bonobon=mysql_fetch_assoc($resultado10);
-										
-										$hola=$bonobon['idInsumos'];
-										echo $hola;
-										$query2 = "Insert into detalle Values ($hola,$ultimoid2)";
-										echo $query2;
-									$resultado10 = mysql_query($query10) or die(mysql_error());
-										}
-										
-										$resultado2 = mysql_query($query2) or die(mysql_error());
-										
-		}
-		else
-		{
-		echo "CTM1<br>";
-		}
-		}
-		else
-		{
-		echo "CTM<br>";
-		}
-		
-		
-}
-}
+        if ($resultado3) {
+            $ultimoid = mysql_fetch_assoc($resultado3);
+            $ultimoid2 = $ultimoid['MAX(idVentas)'];
+            //print_r($ultimoid2);
+            $conteo = 0;
+            foreach ($id2 as $id) {
+
+                // falta un for each para ingresar por cada id ingresado ---------------------------------------------------------------------------------------
+                $query10 = "select idInsumos from Insumos where Codigo = $id";
+                //echo $query10;
+                $resultado10 = mysql_query($query10) or die(mysql_error());
+                $bonobon = mysql_fetch_assoc($resultado10);
+                $hola = $bonobon['idInsumos'];
+                //echo $hola;
+
+
+                $query2 = "Insert into detalle Values ($hola,$ultimoid2)";
+                //echo $query2;
+
+                $resultado2 = mysql_query($query2) or die(mysql_error());
+                if ($resultado2) {
+                    $conteo++;
+                }
+            }//foreach
+            if ($conteo == $cantProductos) {
+                echo 1; // se insertaron todos los prod (detalle)
+            } else {
+                echo 0; // mas o menos inserciones (error)
+            }
+        } else {
+            echo "CTM1<br>";
+        }
+    }// si se inserto la venta
+    else {
+        echo "CTM<br>";
+    }// nose inserto la venta
+}//si no hay profesor
 ?>

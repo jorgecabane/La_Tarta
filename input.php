9f11bd03017adfc70b28bef387d092c04fc8dc0b
nicoride93 <nicoride93@gmail.com>
2013-09-08 20:42:34 -0400

    Update input.php

--- input.php
+++ input.php
@@ -20,12 +20,10 @@
                     }//if
                     else
                     {
-                        
+
                         //manejo del Jason!
-                        prod=$.parseJSON(prod);
-                    $('#carro').prepend('<div codigo="'+prod['Codigo']+'" class="alert alert-info"><span class="label label-info"><strong>'+prod['nombre']+'</strong></span> Precio <span class="label">'+prod['precio']+'</span> <a class="close" data-dismiss="alert" href="#">&times;</a></div>');    
-                  
-                        
+                        prod = $.parseJSON(prod);
+                        $('#carro').prepend('<div codigo="' + prod['Codigo'] + '" class="alert alert-info producto"><span class="label label-info"><strong>' + prod['nombre'] + '</strong></span> Precio <span class="label precio">' + prod['precio'] + '</span> <a class="close" data-dismiss="alert" href="#">&times;</a></div>');
                         $('#error').html('');
                         var pepe = $('#barra');
                         $(document).ready(function() {
@@ -34,21 +32,29 @@
                     }//else
 
                 }//success
-            });//ajax
+            }); //ajax
         }
         //alert(id);
-    });
+    });</script>
+<script>
+    
+
+  $(document).ready(function(){
+      function calcularmonto() {
+
+        var Monto = 0;
+        $('.producto').each(function() {
+            var precio = parseInt($(this).children('.precio').text());
+
+            Monto = Monto + precio;
+        });
+        return Monto;
+    }
+    $('#carro').bind('DOMNodeInserted DOMSubtreeModified DOMNodeRemoved', function(event) {
+       $('#lado').html('<center><h4 style="color: black;">'+calcularmonto()+'</h4></center>');
+       pepe.val('');
+       pepe.focus();
+    })
+})
+
 </script>
-
-
-
-
-
-
-
-
-
-
-
-
-

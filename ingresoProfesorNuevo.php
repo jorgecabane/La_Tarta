6d36226632f919e6af105420c3b0cee65c390b82
nicoride93 <nicoride93@gmail.com>
2013-10-07 19:45:30 -0400

    Update ingresoProfesorNuevo.php

--- ingresoProfesorNuevo.php
+++ ingresoProfesorNuevo.php
@@ -1,30 +1,36 @@
-<?php 
+<?php
+session_start();
+include_once "conexion.php";
+include "header.php";
+if (isset($_SESSION["usuario"])) {
+    ?>
+    <html>
+        <div class="container-fluid">
+            <div class="row-fluid"><div class="well well-small"><a class="btn btn-inverse pull-right">Menu <i class="icon icon-white icon-th-list"></i></a><h3><center>Ingreso Funcionario</center></h3></div></div>
+            <div class="row-fluid">
+                <div class="well well-small">
 
-    session_start();
-    include_once "conexion.php";
-    include "header.php";
-		if (isset($_SESSION["usuario"])) {
-	
-	
-	
-	?>
-	
+                    <table class="table table-bordered table-condensed table-hover table-striped">
 
 
+                        <form name="form1" action="ingresandoProfesor.php" method="POST">
+                            <div><label>Nombre: </label><input name="nombre" type="text" ></div>
+                            <div><label>Apellido: </label><input name="apellido" type="text"></div>
+                            <input type="submit" value="Ingresar">
+                        </form>
+                    </table>
 
-                 <form name="form1" action="ingresandoProfesor.php" method="POST">
-                 <div><label>Nombre: </label><input name="nombre" type="text" ></div>
-                 <div><label>Apellido: </label><input name="apellido" type="text"></div>
-                 <input type="submit" value="Ingresar">
-                 </form>
-	
-                 <div>	<a style="color: #F1F1F1;"href='Latarta.php'/> Volver </a>		</div>
-			
-<?php 
+                    <div>	<a style="color: #F1F1F1;"href='Latarta.php'/> Volver </a>		</div>
 
-}
-else
-{
-echo "Tu no estas autentificado dirigete al <a href='index.php'/> login </a>" ;
-}
-?>
+                    <?php
+                    if ($_SESSION['profe']) {
+                        echo "Has ingresado correctamente a " . $_SESSION['profe'] . " ";
+                    }
+                } else {
+                    echo "Tu no estas autentificado dirigete al <a href='index.php'/> login </a>";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</html>
